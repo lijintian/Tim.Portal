27c7979e350367e1b51db55c416f2d30abd332f9
Tim.Li <2034529669@qq.com>
2015-08-24 22:33:54 +0800

    项目基本分层

---
+++ Tim.Portal.Web/Tim.Portal.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tim.Portal.DataAccess
+{
+    public class Class1
+    {
+    }
+}
